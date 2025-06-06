3e196bb1c92c78c201ba9f6c448054f0d869d19b
pas22ts024 <aleksandr_2004god@icloud.com>
2025-06-06 09:36:06 +0300

    LR1-2

---
+++ lab2/script.js
@@ -0,0 +1,160 @@
+window.onload = function(){ 
+
+    let a = ''
+    let b = ''
+    let expressionResult = ''
+    let selectedOperation = null
+    
+    // окно вывода результата
+    outputElement = document.getElementById("result")
+    
+    // список объектов кнопок циферблата (id которых начинается с btn_digit_)
+    digitButtons = document.querySelectorAll('[id ^= "btn_digit_"]')
+    
+    function onDigitButtonClicked(digit) {
+        if (!selectedOperation) {
+            if ((digit != '.') || (digit == '.' && !a.includes(digit))) { 
+                a += digit
+            }
+            outputElement.innerHTML = a
+        } else {
+            if ((digit != '.') || (digit == '.' && !b.includes(digit))) { 
+                b += digit
+                outputElement.innerHTML = b        
+            }
+        }
+    }
+    
+    // устанавка колбек-функций на кнопки циферблата по событию нажатия
+    digitButtons.forEach(button => {
+        button.onclick = function() {
+            const digitValue = button.innerHTML
+            onDigitButtonClicked(digitValue)
+        }
+    });
+    
+    // установка колбек-функций для кнопок операций
+    document.getElementById("btn_op_mult").onclick = function() { 
+        if (a === '') return
+        selectedOperation = 'x'
+    }
+    document.getElementById("btn_op_plus").onclick = function() { 
+        if (a === '') return
+        selectedOperation = '+'
+    }
+    document.getElementById("btn_op_minus").onclick = function() { 
+        if (a === '') return
+        selectedOperation = '-'
+    }
+    document.getElementById("btn_op_div").onclick = function() { 
+        if (a === '') return
+        selectedOperation = '/'
+    }
+    
+    // кнопка очищения
+    document.getElementById("btn_op_clear").onclick = function() { 
+        a = ''
+        b = ''
+        selectedOperation = ''
+        expressionResult = ''
+        outputElement.innerHTML = 0
+    }
+    
+    // кнопка расчёта результата
+    document.getElementById("btn_op_equal").onclick = function() { 
+        if (a === '' || b === '' || !selectedOperation)
+            return
+            
+        switch(selectedOperation) { 
+            case 'x':
+                expressionResult = (+a) * (+b)
+                break;
+            case '+':
+                expressionResult = (+a) + (+b)
+                break;
+            case '-':
+                expressionResult = (+a) - (+b)
+                break;
+            case '/':
+                expressionResult = (+a) / (+b)
+                break;
+        }
+        
+        a = expressionResult.toString()
+        b = ''
+        selectedOperation = null
+    
+        outputElement.innerHTML = a
+    }
+
+    document.getElementById("btn_op_percent").onclick = function() { 
+        if (a === '' || b === '') return
+
+        b = (+a) * (+b) / 100
+
+        switch(selectedOperation) { 
+            case 'x':
+                expressionResult = (+a) * (+b)
+                break;
+            case '+':
+                expressionResult = (+a) + (+b)
+                break;
+            case '-':
+                expressionResult = (+a) - (+b)
+                break;
+            case '/':
+                expressionResult = (+a) / (+b)
+                break;
+        }
+
+        a = expressionResult.toString()
+        b = ''
+        selectedOperation = null
+        outputElement.innerHTML = a
+    }
+
+    // Keyboard event
+    document.addEventListener(
+        "keydown",
+        (event) =>
+        {
+            switch(event.key)
+            {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                case '.':
+                    onDigitButtonClicked(event.key);
+                    break;
+                case '+':
+                    document.getElementById("btn_op_plus").onclick();
+                    break;
+                case '-':
+                    document.getElementById("btn_op_minus").onclick();
+                    break;
+                case '*':
+                    document.getElementById('btn_op_mult').onclick();
+                    break;
+                case '/':
+                    document.getElementById('btn_op_div').onclick();
+                    break;
+                case '%':
+                    document.getElementById('btn_op_percent').onclick();
+                    break;
+                case 'Enter':
+                    document.getElementById('btn_op_equal').onclick();
+                    break;
+                case 'Backspace':
+                    document.getElementById('btn_op_clear').onclick();
+                    break;
+            }
+        }
+    )
+};
